f8206dee5aea7d0d2c757a13f4491dd80d195a31
Leonardo <leonardosiqueira387@gmail.com>
2023-05-06 19:28:04 -0300

    aaaaaa

---
+++ Sinalizacao.java
@@ -0,0 +1,57 @@
+import java.time.LocalDate;
+
+public class Sinalizacao {
+    
+    private String descricao;
+    private LocalDate dataImplantacao;
+    private int numInicial;
+    private int numFinal;
+    private String lado;
+    private String localDeInstalacao;
+
+    public Sinalizacao(String descricao, LocalDate dataImplantacao, int numInicial, int numFinal, String lado, String localDeInstalacao) {
+        this.descricao = descricao;
+        this.dataImplantacao = dataImplantacao;
+        this.numInicial = numInicial;
+        this.numFinal = numFinal;
+        this.localDeInstalacao = localDeInstalacao;
+        this.lado = lado;
+        this.localDeInstalacao = localDeInstalacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDate getDataImplantacao() {
+        return dataImplantacao;
+    }
+
+    public int getNumInicial() {
+        return numInicial;
+    }
+
+    public int getNumFinal(){
+        return numFinal;
+    }
+
+    public String getLado(){
+        return lado;
+    }
+
+    public String localDeInstalacao(){
+        return localDeInstalacao;
+    }
+
+    public String toString() {
+        return "Sinalização{" +
+                "descricao='" + descricao + '\'' +
+                ", dataImplantacao=" + dataImplantacao +
+                ", numInicial=" + numInicial +
+                ", numFinal=" + numFinal +
+                ", lado='" + lado + '\'' +
+                ", localDeInstalacao='" + localDeInstalacao + '\'' +
+                '}';
+    }
+
+}
