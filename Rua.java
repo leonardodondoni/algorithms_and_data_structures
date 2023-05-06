276e667b12d72950aa1c162a063b0b03793fbd4d
Murilo Vargas <murilo.vargas@qitech.com.br>
2023-05-06 20:15:18 -0300

    add adicionarSinalizacao method in rua class

--- Rua.java
+++ Rua.java
@@ -2,10 +2,20 @@ public class Rua {
     private String s;
     private String sId;
     private String sNome;
+    private ListOfSinalizacao listOfSinalizacao;
 
     public Rua(String s) {
         this.s = s;
         this.sId = s . split (" ", 2) [0];
         this.sNome = s . split (" ", 2) [1];
+        this.listOfSinalizacao = new ListOfSinalizacao();
+    }
+
+    public String getS() {
+        return this.s;
+    }
+
+    public void adicionarSinalizacao(Sinalizacao sinalizacao) {
+        this.listOfSinalizacao.add(sinalizacao);
     }
 }
